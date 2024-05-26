5c77f9e3b2dd9ddf2bacd891216deb7112eb52d9
iyungui <rsdbddml@gmail.com>
2024-05-26 12:45:00 +0900

    Initialize project

---
+++ PhotoKit-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoKit-SwiftUI
+//
+//  Created by Yungui Lee on 5/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoKit-SwiftUI/PhotoKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoKit_SwiftUIApp.swift
+//  PhotoKit-SwiftUI
+//
+//  Created by Yungui Lee on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
